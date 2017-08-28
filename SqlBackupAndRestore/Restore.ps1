f20fe4cafb1d648cbe31e67dc023faf80b42d865
Steven Robijns <steven.robijns@macadam.eu>
2017-08-28 13:42:24 +0200

    Added parameter for path to .bak db backup

--- SqlBackupAndRestore/Restore.ps1
+++ SqlBackupAndRestore/Restore.ps1
@@ -1,15 +1,21 @@
 param(
-	[string] $database
+	[string] $dbName,
+	[string] $dbFilePath
 )
 
-if($database -eq ''){
-	throw "Database parameter not supplied"
+if($dbName -eq ''){
+	throw "dbName parameter not supplied"
 }
 
+if($dbFilePath -eq ''){
+	throw "dbFilePath parameter not supplied"
+}
+
+$dbPath = "`"$dbFilePath`"" 
 . $PSScriptRoot\Configuration.ps1
 
-sqlcmd -S .\dev -U $User -P $Password -i KillConnectionsForDb.sql -v DataBase = "$database"
-sqlcmd -S .\dev -U $User -P $Password -i Restore.sql -v DataBase = "$database" SqlDataFolder = "$SqlDataFolder"
+sqlcmd -S .\dev -U $User -P $Password -i KillConnectionsForDb.sql -v DataBase = "$dbName"
+sqlcmd -S .\dev -U $User -P $Password -i Restore.sql -v DataBase = "$dbName" DbFilePath = "$dbPath" SqlDataFolder = "$SqlDataFolder" 
 
 Write-Host "Press any key to continue ..."
 $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
--- SqlBackupAndRestore/Restore.sql
+++ SqlBackupAndRestore/Restore.sql
@@ -1,3 +1,3 @@
 USE [master]
-RESTORE DATABASE [$(DataBase)] FROM  DISK = N'c:\temp\$(DataBase).bak' WITH  FILE = 1,  MOVE N'$(DataBase)' TO N'$(SqlDataFolder)\$(DataBase).mdf',  MOVE N'$(DataBase)_log' TO N'$(SqlDataFolder)\$(DataBase)_log.LDF',  NOUNLOAD,  REPLACE,  STATS = 5
+RESTORE DATABASE [$(DataBase)] FROM  DISK = N'$(DbFilePath)' WITH  FILE = 1,  MOVE N'$(DataBase)' TO N'$(SqlDataFolder)\$(DataBase).mdf',  MOVE N'$(DataBase)_log' TO N'$(SqlDataFolder)\$(DataBase)_log.LDF',  NOUNLOAD,  REPLACE,  STATS = 5
 GO
