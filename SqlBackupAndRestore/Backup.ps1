eadfdb1b08ec0d1b0a62454b38ae63252c12ec7a
steven.robijns <robijns.steven@gmail.com>
2017-12-18 14:36:34 +0100

    Fixed issue where backup file on file share is in accessible

--- SqlBackupAndRestore/Backup.ps1
+++ SqlBackupAndRestore/Backup.ps1
@@ -23,13 +23,17 @@ if ($ServerForBackup.StartsWith(".")) {
 	$sqlBackupPath = $localBackupFilePath # do not use an intermediate share since we are taking a backup from a local db. 
 } else {
 	$sqlBackupPath = "{0}{1}.bak" -f $BackupShare, $database
+	# remove previous backup file on the backup share	
+	New-PSDrive -Name YDrive -PSProvider filesystem -Root $BackupShare	
+	if (Test-Path YDrive:\PitStop.bak) {
+		Remove-Item YDrive:\PitStop.bak
+	}
 }
 
 sqlcmd -S $ServerForBackup -U $UserForBackup -P $PasswordForBackup -i $PSScriptRoot\Backup.sql -v DataBase = "$database" BackupPath = "$sqlBackupPath"
 
 if ($sqlBackupPath.StartsWith("\\")) {
 	Write-Host "Copying db backup file to local c:\temp This can take a few minutes..."
-	New-PSDrive -Name YDrive -PSProvider filesystem -Root $BackupShare
 	Copy-Item YDrive:\PitStop.bak -Destination "c:\Temp\"
 	Remove-PSDrive -Name YDrive
 }
--- SqlBackupAndRestore/Configuration.ps1
+++ SqlBackupAndRestore/Configuration.ps1
@@ -1,12 +1,16 @@
-# server info to backup from
+# will be used when taking a backup from a remote server so that the server's disk space is preserved
 # -------------------------
-$ServerForBackup = ''
+$BackupShare = '\\YOURBACKUPSERVER\backups\temp\' # make sure it ends with backslash
+# backup configuration
+# -------------------------
+$ServerForBackup = '' # if local server start with .\ e.g.: '.\dev'
 $UserForBackup = ''
 $PasswordForBackup = ''
-# server info to restore to
+$BackupShare = '\\BCKMACADAM01.macadam.local\backups\temp\' # will be used when taking a backup from a remote server so that the server's disk space is preserved
+
+# restore configuration. Only restore to local server is supported for now
 # -------------------------
 $ServerForRestore = ''
 $UserForRestore = ''
 $PasswordForRestore = ''
-# folder where your db's and db log files are located. Check db properties => files in Sql Server
-$SqlDataFolder = "C:\Program Files\Microsoft SQL Server\MSSQL10_50.DEV\MSSQL\DATA"
+$SqlDataFolder = "C:\Program Files\Microsoft SQL Server\MSSQL10_50.DEV\MSSQL\DATA" # folder where your db's and db log files are located. Check db properties => files in Sql Server
